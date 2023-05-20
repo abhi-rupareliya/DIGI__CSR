6db138e9e6b2525c9076f1ae56799263d1bb3af4
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-20 17:38:40 +0530

    Route add of ngo signup

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,9 +2,12 @@ const {
   CompanySignup,
   VerifyCompany,
 } = require("../Controllers/AuthController");
+const { NGOSignup } = require("../Controllers/AuthNGOController");
 
 const AuthRoutes = (app) => {
   app.post("/company/signup", CompanySignup);
   app.post("/company/verify", VerifyCompany);
+  app.post("/ngo/signup", NGOSignup);
+
 };
 module.exports = AuthRoutes;
