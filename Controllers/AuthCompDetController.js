f9d072146bab51e433c28c41c052426984d80710
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-22 12:24:43 +0530

    company detail post route

---
+++ Controllers/AuthCompDetController.js
@@ -0,0 +1,13 @@
+require("dotenv").config({ path: "../.env" });
+const Company = require("../Models/Company");
+const jwt = require("jsonwebtoken");
+
+exports.CompanyDetails = async (req,res) => {
+    try{
+
+    }
+
+    catch(err){
+        
+    }
+}
--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -3,12 +3,14 @@ const {
   VerifyCompany,
 } = require("../Controllers/AuthController");
 const { NGOSignup , VerifyNGO } = require("../Controllers/AuthNGOController");
+const {CompanyDetails} = require("../Controllers/AuthCompDetController");
 
 const AuthRoutes = (app) => {
   app.post("/company/signup", CompanySignup);
   app.post("/company/verify", VerifyCompany);
   app.post("/ngo/signup", NGOSignup);
   app.post("/ngo/verify", VerifyNGO);
+  app.post("/company/details", CompanyDetails);
 
 };
 module.exports = AuthRoutes;
