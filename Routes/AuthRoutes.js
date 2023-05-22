4ba54f416cce2309074583bd79503634f0de50d2
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-22 14:24:57 +0530

    company detail route

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -9,11 +9,11 @@ const AuthRoutes = (app) => {
   app.post("/company/signup", CompanySignup);
   app.post("/company/verify", VerifyCompany);
   app.post("/ngo/signup", NGOSignup);
-<<<<<<< HEAD
-=======
+// <<<<<<< HEAD
+// =======
   app.post("/ngo/verify", VerifyNGO);
   app.post("/company/details", CompanyDetails);
 
->>>>>>> d1ed9d0fe59d62f077c2af5ae5263a622da88578
+// >>>>>>> d1ed9d0fe59d62f077c2af5ae5263a622da88578
 };
 module.exports = AuthRoutes;
