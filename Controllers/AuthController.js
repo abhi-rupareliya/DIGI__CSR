0c2e9240d466225cdac358f64aa0e8a04157f55a
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-22 18:37:14 +0530

    company login route

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -111,3 +111,87 @@ exports.VerifyCompany = async (req, res) => {
     });
   }
 };
+
+  exports.CompanyLogin = async (req, res) => {
+        try{
+            const {email} = req.body;
+
+            const checkEmail = await Company.findOne({ email: email });
+
+            if (!checkEmail) {
+              return res.status(400).send({
+                success: false,
+                message: "Email address is not registered",
+              });
+            }
+
+            const otp = speakeasy.totp({
+              secret: email + process.env.OTPSEC,
+              digits: 6,
+            });
+        
+            const mailOptions = {
+              from: process.env.MAILER,
+              to: email,
+              subject: "OTP VERIFICATION",
+              text: "Your One time password is : " + otp,
+            };
+        
+            transporter.sendMail(mailOptions, (e, info) => {
+              if (e) {
+                res.status(400).send({
+                  success: false,
+                  message: "Error Sending mail.",
+                });
+              } else {
+                res.status(200).send({ success: true, message: "OTP sent" });
+              }
+            });
+          } 
+          
+          catch (error) {
+            res.status(400).send({
+              success: false,
+              message: "Error Sending OTP to your Mail.",
+            });
+          }
+}
+
+  exports.CompanyLoginVerify = async (req, res) => {
+    try {
+      const {email, otp } = req.body;
+  
+      const is_verified = speakeasy.totp.verify({
+        secret: email + process.env.OTPSEC,
+        token: otp,
+        window: 2,
+        encoding: "ascii",
+      });
+  
+      if (is_verified) {
+        // const exist = await Company.findOne({
+        //   $or: [{ email: email }],
+        // });
+  
+        // if (exist) {
+        //   return res.status(400).send({
+        //     success: false,
+        //     message: "Company with this CRN, name or email already exists.",
+        //   });
+        // }
+        // const newCompany = await new Company({ cin, email });
+        // await newCompany.save();
+        const authToken = jwt.sign(
+          { _id: Company._id, email: Company.email },
+          process.env.JWT_SEC
+        );
+        res.status(200).send({ success: true, result: authToken });
+      } else res.status(400).send({ success: false, message: "Wrong OTP" });
+    } catch (error) {
+      console.warn(error);
+      res.status(400).send({
+        success: false,
+        message: "Error in login",
+      });
+    }
+  };
