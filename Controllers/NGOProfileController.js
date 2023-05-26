42e2dd3147cbc1cd8245f9ff20172a7a9a212dbb
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-26 13:45:01 +0530

    changes in ngo profile route

--- Controllers/NGOProfileController.js
+++ Controllers/NGOProfileController.js
@@ -6,9 +6,9 @@ exports.getNGOProfile = async (req,res) => {
         const NGOId = req.params.id;
     
         // Retrieve the NGO document by its ID
-        const NGO = await NGO.findById(NGOId);
+        const ngo = await NGO.findById(NGOId);
     
-        if (!NGO) {
+        if (!ngo) {
           return res
             .status(404)
             .json({ success: false, message: "NGO not found." });
@@ -16,11 +16,22 @@ exports.getNGOProfile = async (req,res) => {
     
         // Prepare the response data
         const responseData = {
-          NGO_name: NGO.NGO_name,
-          email: NGO.email,
-          profile: {
-            
+          NGO_name: ngo.NGO_name,
+          email: ngo.email,
+
+          profile : {
+            summary : ngo.profile.summary,
+            board_members : {
+            bm_name : ngo.profile.board_members.bm_name,
+            bm_gender : ngo.profile.board_members.bm_gender,
+            bm_din : ngo.profile.board_members.bm_din,
+            bm_phone : ngo.profile.board_members.bm_phone,
+            bm_designation : ngo.profile.board_members.bm_designation,
           },
+          csr_budget : ngo.profile.csr_budget,
+          operation_area : ngo.profile.operation_area,
+          sectors : ngo.profile.sectors,
+          }
         };
     
         res.status(200).json({
@@ -33,6 +44,56 @@ exports.getNGOProfile = async (req,res) => {
       }
 };
 
-exports.getNGOCertificate = async (req,res) => {};
+// exports.getNGOCertificate = async (req,res) => {};
 
-exports.AddNGOProfile = async (req,res) => {};
+exports.AddNGOProfile = async (req,res) => {
+  try {
+        const NGOId = req.params.id;
+        const {
+            NGO_name,
+            summary,
+            bm_name,
+            bm_gender,
+            bm_din,
+            bm_phone,
+            bm_designation,
+            csr_budget,
+            operation_area,
+            sectors,
+        } = req.body;
+
+        let updatedFields = {
+            NGO_name,
+            "profile.summary" : summary,
+            "profile.board_members.bm_name" : bm_name,
+            "profile.board_members.bm_gender" : bm_gender,
+            "profile.board_members.bm_din" : bm_din,
+            "profile.board_members.bm_phone" : bm_phone,
+            "profile.board_members.bm_designation" : bm_designation,
+            "profile.csr_budget" : csr_budget,
+            "profile.operation_area" : operation_area,
+            "profile.sectors" : sectors,
+
+        };
+
+    const ngo = await NGO.findByIdAndUpdate(
+      companyId,
+      { $set: updatedFields },
+      { new: true }
+    );
+
+    if (!ngo) {
+      return res
+        .status(404)
+        .json({ success: false, message: "NGO not found." });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: ngo,
+    });
+  }catch(err) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
+};
--- Models/NGO.js
+++ Models/NGO.js
@@ -15,48 +15,50 @@ const ngoSchema = new mongoose.Schema({
     type: String,
   },
 
-  summary: {
-    type: String,
-  },
-
-  board_members: [
-    {
-      bm_name: {
-        type: String,
-      },
-
-      bm_gender: {
-        type: String,
-      },
-
-      bm_din: {
-        type: String,
-      },
-
-      bm_phone: {
-        type: String,
-      },
-
-      bm_designation: {
-        type: String,
-      },
-    },
-  ],
-  csr_budget: {
-    type: Number,
-  },
-
-  operation_area: [
-    {
+  profile : {
+    summary: {
       type: String,
     },
-  ],
-
-  sectors: [
-    {
-      type: String,
+  
+    board_members: [
+      {
+        bm_name: {
+          type: String,
+        },
+  
+        bm_gender: {
+          type: String,
+        },
+  
+        bm_din: {
+          type: String,
+        },
+  
+        bm_phone: {
+          type: String,
+        },
+  
+        bm_designation: {
+          type: String,
+        },
+      },
+    ],
+    csr_budget: {
+      type: Number,
     },
-  ],
+  
+    operation_area: [
+      {
+        type: String,
+      },
+    ],
+  
+    sectors: [
+      {
+        type: String,
+      },
+    ],
+  }
 });
 
 const NGO = mongoose.model("NGO", ngoSchema);
