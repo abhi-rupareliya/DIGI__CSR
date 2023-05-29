042e6e1432797c06cf3736d6c8eff5f071cc877d
jodhanikrunal <21ce045@charusat.edu.in>
2023-05-29 14:09:26 +0530

    route for Beneficiary Login and Signup

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -3,8 +3,10 @@ const {
   VerifyCompany,
 } = require("../Controllers/AuthController");
 const { NGOSignup, VerifyNGO } = require("../Controllers/AuthNGOController");
-const {CompanyLogin , CompanyLoginVerify} = require("../Controllers/AuthController");
-const {NGOLogin , NGOLoginVerify} = require("../Controllers/AuthNGOController");
+const { CompanyLogin, CompanyLoginVerify } = require("../Controllers/AuthController");
+const { NGOLogin, NGOLoginVerify } = require("../Controllers/AuthNGOController");
+const { BeneficiarySignup, VerifyBeneficiary } = require("../Controllers/AuthBeneficiaryController");
+const {BeneficiaryLogin, BeneficiaryLoginVerify} = require("../Controllers/AuthBeneficiaryController");
 
 const AuthRoutes = (app) => {
   app.post("/company/signup", CompanySignup);
@@ -15,5 +17,9 @@ const AuthRoutes = (app) => {
   app.post("/company/login/verify", CompanyLoginVerify);
   app.post("/NGO/login", NGOLogin);
   app.post("/NGO/login/verify", NGOLoginVerify);
+  app.post("/Beneficiary/signup", BeneficiarySignup);
+  app.post("/Beneficiary/verify", VerifyBeneficiary);
+  app.post("/Beneficiary/login", BeneficiaryLogin);
+  app.post("/Beneficiary/login/verify", BeneficiaryLoginVerify);
 };
 module.exports = AuthRoutes;
