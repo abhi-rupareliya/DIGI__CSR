aa3495aef23e2eaa1c66af63fb752243926c8def
sagarnanera <sagarnanera30@gmail.com>
2023-06-03 10:40:53 +0530

    minor changes

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ require("dotenv").config({ path: "../.env" });
 const cors = require("cors");
 const express = require("express");
 
+const logger = require('morgan'); // for logging requests , status codes and more.....
+
 const app = express();
 const PORT = process.env.PORT;
 
@@ -10,6 +12,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" }));
+
+app.use(logger('dev'));
 
 require("./Routes/AuthRoutes")(app);
 require("./Routes/ProfileRoutes")(app);
